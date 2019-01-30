62eb8aba1fdca8d0bf680ad61adaac68c65a5bc8
Erik Tedhamre <erik.tedhamre@student.liu.se>
2019-01-30 09:40:58 +0100

    Realized I need to keep track of original indices to be able to answer

--- intervalcover/intervalcover.cpp
+++ intervalcover/intervalcover.cpp
@@ -16,24 +16,24 @@ vector<int> cover(pair<double, double> interval, vector<pair<double, double> > i
         // Current best interval for current left most point
         vector<pair<double, double> >::iterator best;
         // Second element of best interval
-        double distance_covered = NULL;
+        double distance_covered = 0;
         // Pick the interval that covers the left most point
         // and as much of the rest as possible
-        while( it != intervals.end && left_point >= it->first){
+        while( it != intervals.end() && left_point >= it->first){
             // This can probably be done better
             // We need something to compare with
             // For the first time the point is covered by the interval
-            if (distance_covered == NULL){
+            if (distance_covered == 0){
                 best = it;
-                distance_covered = best->second;
+                distance_covered = abs(left_point - best->second);
             }
-            else if(it->second > distance_covered){
+            else if(abs(left_point - it->second) > distance_covered){
                 best = it;
-                distance_covered = it->second;
+                distance_covered = abs(left_point - best->second);
             }
             it = it + 1;
         }
-        if(distance_covered == NULL){
+        if(distance_covered == 0){
             // Add a -1 as index to the result vector
             // It's impossible to cover
             indices.push_back(-1);
@@ -42,18 +42,23 @@ vector<int> cover(pair<double, double> interval, vector<pair<double, double> > i
         else{
             // Move the left most point
             left_point = best->second;
+            printf("best first %lf, best second %lf \n", best->first, best->second);
             // Add best's index to the result vector
             indices.push_back(best - intervals.begin());
+            int index = best - intervals.begin();
+            printf("best_index %d \n", index);
             // Check if we have covered the entire intervall
             // Is done in the while-loop
             // reset distance_covered
-            distance_covered = NULL;
+            distance_covered = 0;
             // Restart search with new left most point
             // Where we left off
         }  
     }
     if(*indices.end() == -1){
-        return vector<int>{-1};
+        vector<int> bad;
+        bad.push_back(-1);
+        return bad;
     }
     return indices;
 }
@@ -97,16 +102,37 @@ int main() {
         }
         // Sorting in ascending order on first element and then second element
         sort(intervals.begin(), intervals.end(), intervalCmp);
-        vector<int> currentResult = cover(interval, intervals);
+        vector<int> curRes = cover(interval, intervals);
         // Push back current result,
         // unsure if we need to keep it until
         // all input is read or if we can simply print it
+        if(*curRes.end() == -1){
+            result.push_back(-1);
+        }
+        else{
+            result.push_back(curRes.size());
+            result.insert(result.end(), curRes.begin(), curRes.end());
+        }
+        printf("%lu curRes.size() \n", curRes.size());
+        for(int i = 0; i < curRes.size(); i++){
+            if(i == 0){
+                printf("%d number of intervals \n", curRes[0]);
+            }
+            else{
+                printf("%d ", curRes[i]);
+            }
+        }
         /*
         for(int i = 0; i < intervals.size(); i++){
             printf("First: %lf Second: %lf \n", intervals.at(i).first, intervals.at(i).second);
         }
         */
-    }
+    };
+    //
+    //
+    //
+    
+    
 
 
     return 0;
--- intervalcover/intervalcover.md
+++ intervalcover/intervalcover.md
@@ -1,5 +1,7 @@
 # Problem Type
-
+Interval covering
 # Proposed solution
-
+Greedy
+Select the interval that covers the left most point
+and as much as possible of the rest of the interval
 ## Solution complexity
