922615d0411a200443b5ae36ed32cd864f9e5a68
Erik Tedhamre <erik.tedhamre@student.liu.se>
2019-02-14 11:05:15 +0100

    Finished unionfind

--- unionfind/unionfind.cpp
+++ unionfind/unionfind.cpp
@@ -15,35 +15,29 @@ class disjointSet{
 
         // Weighted union for two roots
         void unify(long long a,  long long b){
-            long long parent_a = sets[a];
-            long long parent_b = sets[b];
-
-            // Weighted union if both are roots
-            if(parent_a < 0 && parent_b < 0){
-                long long children;
-                // Parent a has fewer nodes
-                if (parent_a > parent_b){
-                    // Total number of nodes;
-                    children = parent_a + parent_b;
-                    sets[a] = b;
-                    sets[b] = children;
-                }
-                // Parent b has fewer or equal nodes
-                else if(parent_b >= parent_a){
-                    children = parent_a + parent_b;
-                    sets[b] = a;
-                    sets[a] = children;
-                }
+            long long root_a = find(a);
+            long long root_b = find(b);
+            if(root_a == root_b){
+                return;
             }
-            // Both are not roots
-            // a could be a root
-            else if (parent_a > parent_b){
-                sets[b] = a;
+             // Weight of a
+            long long rank_a = sets[root_a];
+             // Weight of b
+            long long rank_b = sets[root_b];
+            // Weighted union 0f both roots
+            long long children;
+            // Parent a has fewer nodes
+            if (rank_a > rank_b){
+                // Total number of nodes;
+                children = rank_a + rank_b;
+                sets[root_a] = root_b;
+                sets[root_b] = children;
             }
-            // Both are not root
-            // b could be root
-            else{
-                sets[a] = b;
+            // Parent b has fewer or equal nodes
+            else if(rank_b >= rank_a){
+                children = rank_a + rank_b;
+                sets[root_b] = root_a;
+                sets[root_a] = children;
             }
         }
 
@@ -55,23 +49,25 @@ class disjointSet{
             long long root = i;
             // loop until root is the root
             // for item i
-            while(sets[root] > 0){
+            vector<long long> nodes;
+            while(sets[root] >= 0){
                 root = sets[root];
             }
             // collapse their depth
             // Go from the bottom up
             // and update roots until
             // you hit the root
+            long long parent;
             while(root != i){
                 // Next node to handle
-                long long parent = sets[i];
+                parent = sets[i];
                 // Set the nodes root to
                 // the collapsed root
                 sets[i] = root;
                 // Update node switch root for
                 i = parent;
             }
-            return root;
+            return parent;
         }
 
         bool same(long long a, long long b){
