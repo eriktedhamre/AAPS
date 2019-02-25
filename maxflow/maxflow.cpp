a5ab2432e62c2713de90ea1d58122487cbffdffc
Erik Tedhamre <erik.tedhamre@student.liu.se>
2019-02-25 10:38:16 +0100

    Started maxflow, going for Dinc's

---
+++ maxflow/maxflow.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+//#define REP(var, k) for (int var = 0; var < (int) k; ++var)
+using namespace std;
+
+//using vi=vector<int>;
+
+struct Edge{
+
+    int dest; // destination node
+
+    int cap; // flow capacity for edge
+
+    int flow; // Current flow
+
+    int source; // Index for edge
+                // connecting to this edge 
+    
+}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // Insert code here
+
+    return 0;
+}
