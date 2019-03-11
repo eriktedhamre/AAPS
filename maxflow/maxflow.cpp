b087d798baba722a4886c5b537fad29deea9677a
Erik Tedhamre <erik.tedhamre@student.liu.se>
2019-03-11 09:58:59 +0100

    Did some more work on maxflow

--- maxflow/maxflow.cpp
+++ maxflow/maxflow.cpp
@@ -12,30 +12,63 @@ static ll const INF = 1000000000000;
 // Struct for representing an edge
 struct Edge{
 
-    ll dest; // destination node
-
-    ll source; // Index for edge
-                // connecting to this edge
-    ll cap; // flow capacity for edge
-
-    ll flow; // Current flow
-
-     
-    
+    // destination for edge
+    ll dest; 
+    // source of edge
+    ll source; 
+    // capacity of edge
+    ll cap;
+    // flow of edge
+    ll flow;
+    // The backwards Edge for this Edge
+    struct Edge *reverse;
 };
 
-vector<vector<Edge> > maxflow(vector<vector<Edge> > network, ll source, ll dest){
+// Used in the function below
+vector<ll> parent; // parent vector
+ll flow; // How much flow we can increase
+// Calculates the maxflow of graph network from s to t
+vector<vector<Edge> > maxflow(vector<vector<Edge> > network, ll s, ll t){
 
-    
+    // Size of network
     ll n = network.size();
-    vector<ll> dist.resize(n, INF);
-    
-    queue<ll> q; q.push(s);
+    // distance in number of nodes from source
+    vector<bool> dist.assign(n, false);
+    dist[s] = true;
+    // parent vector
+    parent.assign(n, -1);
+
+    queue<ll> q;
+    q.push(s);
 
     ll max_flow = 0;
     while(!q.empty()){
         ll u = q.front();
         q.pop();
+
+        // We have reached the sink, break
+        if(u == t){
+        break;
+        }
+        ll neighbours = network[u].size();
+        // For all edges from u
+        for(int i = 0; i < neighbours; i++){
+            // Edge from u
+            Edge edge = network[u][i];
+            // Residual capacity > 0;
+            // and the node is unvisited
+            if(edge.cap - edge.flow > 0 && !dist[edge.dest]){
+                // Node is now visited
+                dist[edge.dest] = true;
+                // Add to queue
+                q.push(edge.dest);
+                // Fix parent
+                parent[edge.dest] = u;
+            }
+        }
+
+        ll f = augment();
+
     }
 }
 
