0143a064254ace87b28b4e56813a31c443bbe8e4
Erik Tedhamre <erik.tedhamre@student.liu.se>
2019-05-08 17:04:23 +0200

    Finished So you want to be a 2^naire

---
+++ 2naire/2naire.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+//#define REP(var, k) for (int var = 0; var < (int) k; ++var)
+using namespace std;
+
+//using vi=vector<int>;
+
+double expectedPrize(int n, double t, int k){
+    if (k == 0){
+        return pow(2, n);
+    }
+    double c_prize = expectedPrize(n, t, k-1);
+    double s_prize = pow(2, n-k);
+    if (t * c_prize > s_prize)
+    {
+        return (((1 - pow(t, 2))*c_prize)/2)/(1-t);
+    }
+    else if (c_prize <= s_prize)
+    {
+        return s_prize;
+    }
+    else
+    {
+        double p0 = s_prize/c_prize;
+        //printf("p0:%lf\n", p0);
+        return ((p0 - t) * s_prize + ((1 - pow(p0, 2))*c_prize)/2)/(1-t); 
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    // Insert code here
+    int n;
+    double t;
+    while(scanf("%d %lf", &n, &t)){
+
+        if (n == 0 && t == 0) {
+            break;
+        }
+
+        printf("%.3lf\n", expectedPrize(n ,t, n));
+    }
+    return 0;
+}
