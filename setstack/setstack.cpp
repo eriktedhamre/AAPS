caedfb43b2c2b75c1c59477e20aa77685c92c476
Erik <erik.tedhamre@gmail.com>
2024-02-22 20:49:04 +0100

    Got some help

--- setstack/setstack.cpp
+++ setstack/setstack.cpp
@@ -1,6 +1,8 @@
 
 #include <variant>
 #include <vector>
+#include <iostream>
+#include <stack>
 
 struct VariableDepthList : std::variant<std::vector<VariableDepthList>, int> {
 private:
@@ -12,6 +14,11 @@ public:
 
 // This does not look right
 
+/*
+ * Got some help :) 
+ * Algorithm: give each set a unique integer ID, represent a set of
+ *            sets as a set of IDs.
+*/
 typedef struct setstack;
 
 struct setstack
@@ -19,3 +26,47 @@ struct setstack
     int value;
     std::vector<setstack> list;
 };
+
+class Computer {
+public:
+    // Constructor
+    Computer() : {
+
+    }
+
+    void PUSH() {
+        stack.push_back()
+    }
+
+    void DUP() {
+
+    }
+
+    void UNION() {
+
+    }
+
+    void INTERSECT() {
+
+    }
+
+    void ADD() {
+
+    }
+
+    void clear() {
+        stack.clear();
+    }
+
+private:
+    std::vector<setstack> stack;
+
+};
+
+int main() {
+    // Creating an instance of SetStack
+    Computer obj(42);
+
+
+    return 0;
+}
