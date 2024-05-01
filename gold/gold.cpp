d3976b438cf4188d8bbcebf9b370d9fb666786c6
Erik Tedhamre <thedis95@gmail.com>
2024-05-01 17:25:22 +0200

    Clean up gold.cpp for presentation

--- gold/gold.cpp
+++ gold/gold.cpp
@@ -1,23 +1,16 @@
 #include <bits/stdc++.h>
-//#define REP(var, k) for (int var = 0; var < (int) k; ++var)
 using namespace std;
 
-//using vi=vector<int>;
-
+// Treasure map
 vector<vector<char> > t_map;
-
 // Keep track of breeze squares 
 vector<vector<bool> > breeze;
-
-// Visited list
+// Visited
 vector<vector<bool> > visited;
-
-// possible moves
+// Possible moves
 vector<pair<int, int> > moves{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
     
     pair<int, int> P;
     int col, row;
@@ -29,20 +22,13 @@ int main() {
     visited.resize(col, vector<bool> (row, false));
 
     char token;
-    // while-loop tills vi får ett korrekt tecken
-    // isspace
-    //
-    //
-    for(int i = 0; i < row; i++)
-    {
-        for(int j = 0; j < col; j++)
-        {
+    for(int i = 0; i < row; i++) {
+        for(int j = 0; j < col; j++) {
+
             scanf("%c", &token);
-            while(isspace(token)){
-                scanf("%c", &token);
-            }
-            switch (token)
-            {
+            while(isspace(token)) { scanf("%c", &token); }
+
+            switch (token) {
                 case '.':
                     t_map[j][i] = '.';
                     break;
@@ -52,10 +38,8 @@ int main() {
                     break;
                 case 'T':
                     t_map[j][i] = 'T';
-                    breeze[j + 1][i] = true;
-                    breeze[j - 1][i] = true;
-                    breeze[j][i + 1] = true;
-                    breeze[j][i - 1] = true;
+                    breeze[j + 1][i] = true; breeze[j - 1][i] = true;
+                    breeze[j][i + 1] = true; breeze[j][i - 1] = true;
                     break;
                 case 'G':
                     t_map[j][i] = 'G';
@@ -65,16 +49,15 @@ int main() {
                     P = make_pair(j, i);
                     visited[j][i] = true;
                     break;         
-                default:
-                    break;
             }
         }
     }
 
-    queue<pair<int, int> > q; q.push(P);
+    queue<pair<int, int> > q;
+    q.push(P);
 
     int gold = 0;
-    while(!q.empty()){
+    while(!q.empty()) {
         pair<int, int> s = q.front();
         q.pop();
         
@@ -84,10 +67,14 @@ int main() {
         if(breeze[s.first][s.second])
             continue;
 
-        for(int i = 0; i < 4; i++)
-        {
+        for(int i = 0; i < 4; i++) {
+
             pair<int, int> next = make_pair(s.first + moves[i].first, s.second + moves[i].second);
-            if((next.first < col)&&(next.first > 0)&&(next.second > 0)&&(next.second < row)&&(!visited[next.first][next.second])){
+
+            if((next.first < col) && (next.first > 0) &&
+               (next.second > 0)  && (next.second < row) &&
+               (!visited[next.first][next.second])) {
+
                 q.push(next);
                 visited[next.first][next.second] = true;
             }
